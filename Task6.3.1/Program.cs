49e70a22cce87218dd3c74c41f827aaba9ae4999
Bochanikita <bochanikita@yandex.ru>
2022-08-27 11:40:30 +0300

    Add task 6.5.2

--- Task6.3.1/Program.cs
+++ Task6.3.1/Program.cs
@@ -1,38 +1,38 @@
 ﻿class Company
 {
-	public string Type;
-	public string Name;
+    public string Type = "Банк";
+    public string Name = "sdf";
 }
 
 class Department
 {
-	public Company Company;
-	public City City;
-
-
+    public Company Company;
+    public City City;
 }
 
 class City
 {
-	public string Name;
+    public string Name = "Санкт-Петербург";
 }
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		var department = GetCurrentDepartment();
-		if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
-		{
-			Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
-		}
-	}
+    static void Main(string[] args)
+    {
+        var department = GetCurrentDepartment();
+        if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+        {
+            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге",
+                department?.Company?.Name ?? "Неизвестная компания");
+        }
+    }
 
-	static Department GetCurrentDepartment()
-	{
-		Department department = new Department();
-		department.Company.Type = "Банк";
-		department.City.Name = "Москва";
-		return department;
-	}
+    static Department GetCurrentDepartment()
+    {
+        Department department = new Department();
+        department.Company.Type = "Банк";
+        department.Company.Name = "123";
+        department.City.Name = "Санкт-Петербург";
+        return department;
+    }
 }
---
+++ Task6.5.2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Circle
+{
+	public double radius;
+
+	public double Square()
+	{
+	}
+
+	public double Length()
+	{
+	}
+}
+
+class Triangle
+{
+	public int a;
+	public int b;
+	public int c;
+
+	public double Square()
+	{
+	}
+
+	public double Perimeter()
+	{
+	}
+}
+
+class Square
+{
+	public int side;
+
+	public double Square()
+	{
+	}
+
+	public double Perimeter()
+	{
+	}
+}
