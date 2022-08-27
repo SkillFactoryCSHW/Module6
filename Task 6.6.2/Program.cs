5f30603196de0e5d2f373e98d9e4c37f71f6fc1b
Bochanikita <bochanikita@yandex.ru>
2022-08-27 12:20:50 +0300

    Add task 6.6.2

---
+++ Task 6.6.2/Program.cs
@@ -0,0 +1,46 @@
+﻿class User
+{
+	private string login;
+
+	public string Login
+	{
+		get
+		{
+			return login;
+		}
+
+		set
+		{
+			if (value.Length < 3)
+			{
+				Console.WriteLine("Логин должен быть длиной от 3 символов");
+			}
+			else
+			{
+				login = value;
+			}
+		}
+	}
+
+	private string email;
+
+	public string Email
+	{
+		get
+		{
+			return email;
+		}
+
+		set
+		{
+			if (!value.Contains('@'))
+			{
+				Console.WriteLine("Неверный формат адреса электронной почты");
+			}
+			else
+			{
+				email = value;
+			}
+		}
+	}
+}
