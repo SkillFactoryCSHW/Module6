a0f9388b97c37133e6b1ebc90d04654680622b17
Bochanikita <bochanikita@yandex.ru>
2022-08-27 11:11:33 +0300

    Add method GetCurrentDepartment

--- Task6.3.1/Program.cs
+++ Task6.3.1/Program.cs
@@ -8,9 +8,31 @@ class Department
 {
 	public Company Company;
 	public City City;
+
+
 }
 
 class City
 {
 	public string Name;
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var department = GetCurrentDepartment();
+		if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+		{
+			Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+		}
+	}
+
+	static Department GetCurrentDepartment()
+	{
+		Department department = new Department();
+		department.Company.Type = "Банк";
+		department.City.Name = "Москва";
+		return department;
+	}
 }
