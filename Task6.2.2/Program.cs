362eabc6830a11f8d34a5049d915c1a424d601eb
Bochanikita <bochanikita@yandex.ru>
2022-08-27 10:30:27 +0300

    Add constructors

--- Task6.2.2/Program.cs
+++ Task6.2.2/Program.cs
@@ -2,4 +2,18 @@
 {
 	public string color;
 	public int cost;
+
+	public Pen()
+	{
+		color = "Черный";
+		cost = 1;
+	}
+
+	public Pen(string penColor, int penCost)
+    {
+		color = penColor;
+		cost = penCost;
+    }
 }
+
+
