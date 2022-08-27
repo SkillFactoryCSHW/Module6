a948e33e3755e4fcc0dc65c040da09f054b7e029
Bochanikita <bochanikita@yandex.ru>
2022-08-27 10:45:44 +0300

    Add constructors

---
+++ Task6.2.8/Program.cs
@@ -0,0 +1,26 @@
+﻿class Rectangle
+{
+    public int a, b;
+    public int Square()
+    {
+        return a * b;
+    }
+
+    public Rectangle()
+    {
+        a = 10;
+        b = 5;
+    }
+
+    public Rectangle(int side)
+    {
+        a = side;
+        b = side;
+    }
+
+    public Rectangle(int first, int second)
+    {
+        a = first;
+        b = second;
+    }
+}
